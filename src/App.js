a73186daaa26785f7ef37d040e899da332931117
Aayush <aayushsoni237@gmail.com>
2023-02-05 16:04:18 +0530

    Lecture 12

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import './App.css';
 import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import React, { useState } from 'react';
+
 
 function App() {
+  const [mode, setMode] = useState("light"); //Whether dark mode is enabled or not
+  
+  const toggleMode = () => {
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "#0e1863";
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
   <>
-      <Navbar title="textUtils" aboutText="about me"/>
+      {/* <Navbar title="textUtils" aboutText="about me"/> */}
+      <Navbar title="textUtils" mode={mode} toggleMode={toggleMode} aboutText="About"/>
       <div className='container my-3'>
       {/* <About/> */}
-      <TextForm heading="Enter the text to be analysed"/>
+      <TextForm heading="Enter the text to be analysed" mode={mode}/>
       </div>
   </>
   );
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -33,12 +33,13 @@ export default function TextForm(props) {
     const [text, setText] = useState("");
   return (
     <>
-    <div>
+    <div className="container" style={{color: props.mode==="dark"?"white":"black"}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea
             className="form-control" value={text}
-            id="textBox" onChange={handleOnChange}
+            id="textBox" style={{backgroundColor: props.mode==="dark"?"gray":"white", color: props.mode==="dark"?"white":"black"}} 
+            onChange={handleOnChange}
             rows="8"
           ></textarea>
         </div>
@@ -46,13 +47,13 @@ export default function TextForm(props) {
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lower case</button>
         <button className="btn btn-primary" onClick={handleSen}>Count sentences</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==="dark"?"white":"black"}}>
         <h2>Your text summary:</h2>
         <p>Your text has {text.split(" ").length} words and {text.length} characters.</p>
         <p>It is a {0.008 * text.split(" ").length} minutes long read.</p>
         <p>Your text has {count} sentences.</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text: "Enter text in the textbox above to preview"}</p>
     </div>
     </>
   );
