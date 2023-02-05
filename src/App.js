41d790d514be8309ab0293172839bbd2d93a244e
Aayush <aayushsoni237@gmail.com>
2023-02-05 11:46:02 +0530

    Lecture 11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 
@@ -8,6 +9,7 @@ function App() {
   <>
       <Navbar title="textUtils" aboutText="about me"/>
       <div className='container my-3'>
+      {/* <About/> */}
       <TextForm heading="Enter the text to be analysed"/>
       </div>
   </>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,6 +1,8 @@
 import React,{useState} from "react";
 
 export default function TextForm(props) {
+  let sentenceCount = 0; let [count, setCount] = useState(0);
+
   const handleUpClick = () => {
     // console.log("You clicked the button: " + text);
     let UCText =  text.toUpperCase();
@@ -18,6 +20,15 @@ export default function TextForm(props) {
     setText(event.target.value);
   }
 
+  const handleSen = () => {
+    for(count=0; count<text.length; count++){
+      if(text.charAt(count).match(/[.?!]/)){
+        sentenceCount++; 
+        setCount(sentenceCount);
+      }
+    }
+  }
+
 
     const [text, setText] = useState("");
   return (
@@ -32,12 +43,14 @@ export default function TextForm(props) {
           ></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to upper case</button>
-        <button className="btn btn-primary" onClick={handleLoClick}>Convert to lower case</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to lower case</button>
+        <button className="btn btn-primary" onClick={handleSen}>Count sentences</button>
     </div>
     <div className="container my-3">
         <h2>Your text summary:</h2>
         <p>Your text has {text.split(" ").length} words and {text.length} characters.</p>
         <p>It is a {0.008 * text.split(" ").length} minutes long read.</p>
+        <p>Your text has {count} sentences.</p>
         <h2>Preview</h2>
         <p>{text}</p>
     </div>
